d97aa4ccffaa6da06cfc8d9decb8074e4166be2a
j.rrodriguez1 <j.rrodriguez1@udc.es>
2023-03-04 11:31:45 +0100

    Empezar vote

--- CLion/Plantilla-Template_P1/P1/main.c
+++ CLion/Plantilla-Template_P1/P1/main.c
@@ -36,18 +36,53 @@ void New (tParticipantName countryname, tEUParticipant euParticipant, tList *l) 
       } else {
         printf("* New: participant %s location non-eu\n", participant.participantName);
       }
+    } else {
+      printf("+ Error: New not possible\n");
     }
   }
+}
+
+/*void Vote (tParticipantName participant, tNumVotes, )*/
+
+void Stats (tList l) {
+  int s = 0, p = 0;
+  float v_n = 0,v_e = 0;
+  float c_n = 0, c_e = 0;
+  tPosL pos;
+  tItemL item;
+
+  if(isEmptyList(l)) {
+    printf("+ Error: Stats not possible\n");
+  } else {
+    pos = first(l);
+    while(pos != LNULL) {
+      item = getItem (pos, l);
+      if (item.EUParticipant) {
+        p++;
+        c_e = item.numVotes + c_e;
+        v_e = c_e/p;
+        printf("Participant %s location eu numvotes %d (%.2f)\n", item.participantName, item.numVotes, v_e);
+      } else {
+        s++;
+        c_n = item.numVotes + c_n;
+        v_n = c_n/s;
+        printf("Participant %s location non-eu numvotes %d (%.2f)\n", item.participantName, item.numVotes, v_n);
+      }
+      pos = next(pos, l);
+    }
+  }
+
 }
 
 void processCommand(char *commandNumber, char command, char *param1, char *param2, tList *l) {
   printf("********************\n");
   switch (command) {
     case 'N':                 //realiza new
-      printf("%s %c %s %s\n", commandNumber, command, param1, param2);
+      printf("%s %c: participant %s location %s\n", commandNumber, command, param1, param2);
       New(param1, param2, l);
       break;
     case 'V':                 //realiza vote
+      printf("%s %c: ");
       break;
     case 'D':                 //realiza disqualify
       break;
