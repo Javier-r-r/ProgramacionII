d84b4cf05cf987ce681680352f596d9e2e324fce
j.rrodriguez1 <j.rrodriguez1@udc.es>
2023-03-12 10:42:56 +0100

    P1 Terminada 100%

--- CLion/Plantilla-Template_P1/P1/dynamic_list.c
+++ CLion/Plantilla-Template_P1/P1/dynamic_list.c
@@ -23,15 +23,10 @@ bool isEmptyList(tList L){              //Objetivo: Determinar si la lista está
   return(L == LNULL);                   //Entrada: La lista a comprobar
 }                                       //Salida: True si la lista está vacía, false en caso contrario
 
-tItemL getItem(tPosL p,tList L){        //Objetivo: Devolver el contenido del elemento de la lista que ocupa la posición indicada
-  return p -> data;                     //Entrada: Posición de la lista, la lista
-}                                       //Salida: El dato tItemL que está en la posición indicada
-                                        //PreCD: La posición indicada es una posición válida en la lista
-
 tPosL first(tList L){                   //Objetivo: Devolver la posición del primer elemento de la lista
   return L;                             //Entrada: Lista de la que se quiere conseguir la primera posición
 }                                       //Salida: La primera posición de la lista
-                                        //PreCD: La lista no está vacía
+                                        // PreCD: La lista no está vacía
 
 tPosL last(tList L){                    //Objetivo: Devolver la posición del último elemento de la lista
   tPosL p;                              //Entrada: La lista de la que se quiere conseguir la última posición
@@ -40,10 +35,10 @@ tPosL last(tList L){                    //Objetivo: Devolver la posición del ú
 }
 
 tPosL next( tPosL p, tList L){          //Objetivo: Devolver la posición en la lista del elemento siguiente al de la posición indicada
-    return p->next;                     //Entrada: Posición de la que se quiere saber la siguiente, Lista correspondiente
-                                        //Salida: Posición siguiente (o LNULL si la posición no tiene siguiente)
+  return p->next;                       //Entrada: Posición de la que se quiere saber la siguiente, Lista correspondiente
+}                                       //Salida: Posición siguiente (o LNULL si la posición no tiene siguiente)
                                         //PreCD: La posición indicada es una posición válida en la lista
-}
+
 
 tPosL previous(tPosL p, tList L){       //Objetivo: Devolver la posición en la lista del elemento anterior al de la posición indicada
   if(p == first(L)){                    //Entrada: Posición de la que se quiere saber la anterior, Lista correspondiente
@@ -53,19 +48,6 @@ tPosL previous(tPosL p, tList L){       //Objetivo: Devolver la posición en la 
     return q;
   }
   tPosL q;
-}
-
-tPosL findItem(tParticipantName d, tList L){    //Objetivo: Devolver la posición del primer elemento de la lista cuyo nombre de
-  tPosL p;                                      //participante se corresponda con el indicado
-  if (isEmptyList(L)) {                         //Entrada: Nombre del participante a encontrar, Lista correspondiente
-    return LNULL;                               //Salida: La posición que ocupa dicho participante en la lista (o LNULL si no existe tal participante)
-  } else {
-    for(p = L;p != LNULL && (strcmp(p -> data.participantName, d) < 0); p = p -> next);
-    if(p != LNULL && strcmp(p -> data.participantName, d) == 0)
-      return p;
-    else
-      return LNULL;
-  }
 }
 
 bool insertItem(tItemL d,tPosL p,tList* L){     //Objetivo: Insertar un elemento en la lista antes de la posición indicada. Si la posición es LNULL, entonces se añade al final.
@@ -110,8 +92,31 @@ void deleteAtPosition(tPosL p, tList *L){       //Objetivo: Eliminar de la lista
   }
   free(p);
 }
+
+tItemL getItem(tPosL p,tList L){        //Objetivo: Devolver el contenido del elemento de la lista que ocupa la posición indicada
+  return p -> data;                     //Entrada: Posición de la lista, la lista
+}                                       //Salida: El dato tItemL que está en la posición indicada
+                                        //PreCD: La posición indicada es una posición válida en la lista
+
 void updateItem(tItemL d, tPosL p, tList* L){   //Objetivo: Modificar el contenido del elemento situado en la posición indicada
   p -> data = d;                                //Entrada: Dato tItemL que contiene la nueva información del participante, Posición donde se encuentra el participante, Lista donde se modifica la información
 }                                               //Salida: Lista con la información modificada
                                                 //PreCD: La posición indicada es una posición válida en la lista
                                                 //PostCD: El orden de los elementos de la lista no se ve modificado
+
+tPosL findItem(tParticipantName d, tList L){    //Objetivo: Devolver la posición del primer elemento de la lista cuyo nombre de
+  tPosL p;                                      //participante se corresponda con el indicado
+  if (isEmptyList(L)) {                         //Entrada: Nombre del participante a encontrar, Lista correspondiente
+    return LNULL;                               //Salida: La posición que ocupa dicho participante en la lista (o LNULL si no existe tal participante)
+  } else {
+    for(p = L;p != LNULL && (strcmp(p -> data.participantName, d) < 0); p = p -> next);
+    if(p != LNULL && strcmp(p -> data.participantName, d) == 0)
+      return p;
+    else
+      return LNULL;
+  }
+}
+
+
+
+
